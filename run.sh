c85ceefdf69a0227f9dfeeffa89047027e056081
Pete Keen <pete@petekeen.net>
2020-06-26 20:25:24 -0400

    wait for zt id gen

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ set -e
 set -x
 set -o pipefail
 
+echo '*** waiting for zerotier identity generation...'
+
+while [ ! -f /var/lib/zerotier-one/identity.secret ]; do
+	sleep 1
+done
+
 zerotier-cli join $ZT_NETOWRK
 
 while true; do
